ebcdab6235d4bfff77fbfc03f0bb22ced288b745
Youngung Jeong <yjeong@changwon.ac.kr>
2025-07-29 08:50:37 +0900

    Updates in <voigt.py> and added <chg_basis.py>

--- src/mat/voigt.py
+++ src/mat/voigt.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 ijv = np.zeros((2,6),dtype='int')
 ijv[0,0],ijv[1,0] = 0,0
 ijv[0,1],ijv[1,1] = 1,1
@@ -13,9 +14,34 @@ vij[1,1] = 1
 vij[2,2] = 2
 vij[1,2] = 3
 vij[0,2] = 4
-vij[0,1] = 5 
+vij[0,1] = 5
 
 # lower triangle
 vij[2,1] = 3
 vij[2,0] = 4
-vij[1,0] = 5 
+vij[1,0] = 5
+
+def voigt(aux33,aux6,iopt=0):
+    """
+    iopt 0: aux33 (Cart.) -> aux6  (Voigt)
+    returns aux6
+
+    iopt 1: aux6  (Voigt) -> aux33 (Cart.)
+    returns aux33
+    """
+    if iopt==0:
+        rst6=np.zeros(6)
+        for iv in range(6):
+            i0,j0=ijv[:,iv]
+            rst6[iv]=aux33[i0,j0]
+        return rst6
+    elif iopt==1:
+        rst33=np.zeros((3,3))
+        for iv in range(6):
+            i0,j0=ijv[:,iv]
+            rst33[i0,j0]=aux6[iv]
+            rst33[j0,i0]=aux33[i0,j0]
+        return rst33
+    else:
+        sys.stderr(f'** Error: unexpected iopt <{iopt}> was given\n')
+        sys.exit(-1)
